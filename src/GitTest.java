5e57346e5c4b92cc02dabf7cae25b3700b07e43b
Max <psylocke359@gmail.com>
2021-09-30 20:19:08 +0300

    added array

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,10 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        int[] array = new int[10];
+        for (int i = 0; i< array.length;i++) {
+            array[i] = i*10;
+            System.out.print(array[i]+"  ");
+        }
     }
 }
